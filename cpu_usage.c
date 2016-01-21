5896e05373f528029317f82135651751a7173e69
Jian-Hui Lee <wespe.ljh@gmail.com>
2016-01-21 13:44:48 +0800

    Exit program if process under monitored is gone.

--- cpu_usage.c
+++ cpu_usage.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 struct string {
     char *buf;
@@ -40,6 +42,38 @@ int string_init(struct string *str, size_t claim)
 
     buf[0] = '\0';
     str->buf = buf;
+    str->size = claim;
+
+    return 0;
+}
+
+int read_first_line_cpu(char *fn, struct string *str)
+{
+    FILE *fp = NULL;
+
+    fp = fopen(fn, "r");
+
+    if (!fp) {
+        printf("Open %s fail: %s\n", fn, strerror(errno));
+        return -1;
+    }
+
+    rewind(fp);
+    fflush(fp);
+    str->buf[0] = '\0';
+    /* Read one line */
+    while (!feof(fp)) {
+        if (fgets(str->buf, (int)str->size, fp) != NULL) {
+            if (strncmp(str->buf, "cpu ", 4) == 0) 
+                break;
+            else
+                printf("Strange case...%s\n", str->buf);
+        }
+        else {
+            perror("fgets");
+        }
+    }
+    fclose(fp);
 
     return 0;
 }
@@ -55,12 +89,13 @@ int read_first_line(char *fn, struct string *str)
         return -1;
     }
 
+    rewind(fp);
+    fflush(fp);
+    str->buf[0] = '\0';
     /* Read one line */
     if (fgets(str->buf, str->size, fp) == NULL) {
-        printf("Read line data fail: %s filename: %s\n", strerror(errno), fn);
-        return -1;
+        printf("DEBUG NULL %s\n", fn);
     }
-
     fclose(fp);
 
     return 0;
@@ -74,7 +109,7 @@ int parse_system(struct string *str, struct cpu_info *info)
                     &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10);
 
     if (rc != 10) {
-        printf("sscanf error!\n");
+        printf("sscanf error! %s\n", str->buf);
         return -1;
     }
 
@@ -110,7 +145,7 @@ int parse_process(struct string *str, struct cpu_info *info)
 
 int fetch_data(struct entry_data *en)
 {
-    read_first_line(en->system_e.filename, &en->system_e.data);
+    read_first_line_cpu(en->system_e.filename, &en->system_e.data);
     read_first_line(en->process_e.filename, &en->process_e.data);
 
     return 0;
@@ -148,6 +183,54 @@ int init(int pid, struct entry_data *en)
     return 0;
 }
 
+pid_t run_cmd(char *const *cmd)
+{
+    pid_t pid;
+
+    pid = fork();
+    if (pid < 0) {
+        printf("Error fork: %s\n", strerror(errno));
+        return -1;
+    }
+    else if (pid == 0) {
+        /* Child */
+        execvp(cmd[0], cmd);
+        printf("Child error exec!\n");
+        exit(0);
+    }
+
+    /* Parent */
+    return pid;
+}
+
+int check_process_alive(int pid)
+{
+    int status, rc;
+
+    rc = waitpid(pid, &status, WNOHANG);
+
+    if (rc == pid) {
+        if (WIFEXITED(status)) {
+            printf("exited, status = %d\n", WEXITSTATUS(status));
+            return 0;
+        }
+        else if (WIFSIGNALED(status)) {
+            printf("killed by signal %d\n", WTERMSIG(status));
+            return 0;
+        }
+        else if (WIFSTOPPED(status)) {
+            printf("stopped by signal %d\n", WSTOPSIG(status));
+            return 0;
+        }
+        else if (WIFCONTINUED(status)) {
+            printf("continued\n");
+            return 1;
+        }
+    }
+
+    return 1;
+}
+
 int main(int argc, const char *argv[])
 {
     int mpid = 0;
@@ -155,10 +238,16 @@ int main(int argc, const char *argv[])
     struct cpu_usage first;
     struct cpu_usage second;
 
+#if 1
+    if (argv[1]) {
+        mpid = run_cmd((char * const*)&argv[1]);
+    }
+#else
     mpid = atoi(argv[1]);
+#endif
     init(mpid, &data);
 
-    while (1) {
+    while (check_process_alive(mpid)) {
         fetch_data(&data);
         analyze(&data, &first);
 
@@ -167,6 +256,7 @@ int main(int argc, const char *argv[])
         analyze(&data, &second);
 
         show_usage(&first, &second);
+
         sleep(1);
     }
     
